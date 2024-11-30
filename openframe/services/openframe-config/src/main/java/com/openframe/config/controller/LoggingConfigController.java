e3559ce4f8715aa25f194a3ea6017b94dbaabdcf
Michael Assraf <michaelassraf@gmail.com>
2024-11-30 10:43:26 -0500

    aa

--- openframe/services/openframe-config/src/main/java/com/openframe/config/controller/LoggingConfigController.java
+++ openframe/services/openframe-config/src/main/java/com/openframe/config/controller/LoggingConfigController.java
@@ -3,6 +3,7 @@ package com.openframe.config.controller;
 import java.nio.charset.StandardCharsets;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +17,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/logging")
 public class LoggingConfigController {
 
+    @Value("${log.config-server-url}")
+    private String configServerUrl;
+
     @GetMapping(value = "/{filename:.+}", produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<String> getLoggingConfig(@PathVariable String filename) throws Exception {
         ClassPathResource resource = new ClassPathResource("logging/" + filename);
         String content = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-        content = StringUtils.replace(content, "logging/", "/logging/");
+        content = StringUtils.replace(content, "logging/", configServerUrl + "/logging/");
         return ResponseEntity.ok()
                 .contentType(MediaType.APPLICATION_XML)
                 .body(content);
