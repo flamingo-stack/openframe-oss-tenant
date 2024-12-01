44b8240b44f55fd0d154e4f207dec84a56fa6a21
Michael Assraf <michaelassraf@gmail.com>
2024-12-01 11:47:31 -0500

    fix

--- openframe/libs/openframe-data/src/main/java/com/openframe/data/config/RedisConfig.java
+++ openframe/libs/openframe-data/src/main/java/com/openframe/data/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.openframe.core.config;
+package com.openframe.data.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- openframe/libs/openframe-data/src/main/java/com/openframe/data/repository/mongo/EventRepository.java
+++ openframe/libs/openframe-data/src/main/java/com/openframe/data/repository/mongo/EventRepository.java
@@ -1,10 +1,12 @@
-package com.openframe.core.repository;
+package com.openframe.data.repository.mongo;
 
-import com.openframe.core.model.Event;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import java.time.Instant;
 import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import com.openframe.core.model.Event;
 
 public interface EventRepository extends MongoRepository<Event, String> {
     List<Event> findByUserIdAndTimestampBetween(String userId, Instant start, Instant end);
