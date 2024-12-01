ea3f6254ed18bc19c7513226732af61a8cacd50f
Michael Assraf <michaelassraf@gmail.com>
2024-12-01 17:34:28 -0500

    כןס

--- openframe/libs/openframe-data/src/main/java/com/openframe/data/config/CassandraConfig.java
+++ openframe/libs/openframe-data/src/main/java/com/openframe/data/config/CassandraConfig.java
@@ -1,5 +1,6 @@
 package com.openframe.data.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
@@ -10,8 +11,16 @@ import org.springframework.data.cassandra.repository.config.EnableCassandraRepos
 @EnableCassandraRepositories(basePackages = "com.openframe.data.repository.cassandra")
 public class CassandraConfig extends AbstractCassandraConfiguration {
 
+    @Value("${spring.data.cassandra.local-datacenter}")
+    private String localDatacenter;
+
     @Override
     protected String getKeyspaceName() {
         return "openframe";
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return localDatacenter;
     }
 }
