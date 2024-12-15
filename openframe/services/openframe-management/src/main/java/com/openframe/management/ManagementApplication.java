ef92c630531852294aa79b1dda862db5616a0542
Michael Assraf <michaelassraf@gmail.com>
2024-12-15 17:01:17 -0500

    fix

--- openframe/services/openframe-management/src/main/java/com/openframe/management/ManagementApplication.java
+++ openframe/services/openframe-management/src/main/java/com/openframe/management/ManagementApplication.java
@@ -3,8 +3,10 @@ package com.openframe.management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.openframe.data.repository")
 @ComponentScan(basePackages = {"com.openframe.management", "com.openframe.data", "com.openframe.core"})
 public class ManagementApplication {
     public static void main(String[] args) {
