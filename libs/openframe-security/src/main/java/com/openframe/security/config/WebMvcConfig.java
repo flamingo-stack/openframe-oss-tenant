00522578fe87683578e77f55994f8037e261642c
Michael Assraf <michaelassraf@gmail.com>
2024-12-25 10:56:29 -0500

    fix

--- libs/openframe-security/src/main/java/com/openframe/security/config/WebMvcConfig.java
+++ libs/openframe-security/src/main/java/com/openframe/security/config/WebMvcConfig.java
@@ -1,10 +1,14 @@
 package com.openframe.security.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
+@ConditionalOnProperty(name = "spring.main.web-application-type", havingValue = "servlet")
 public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
