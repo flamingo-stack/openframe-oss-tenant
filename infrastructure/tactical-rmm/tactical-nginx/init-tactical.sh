dcb8ba31d39144a9a9c3e7754bf39da0a8e65b84
Vusal Aliyev <vusal@flamingo.cx>
2025-03-20 20:24:03 +0400

    [k8s] improve tactical nginx

--- infrastructure/tactical-rmm/tactical-nginx/init-tactical.sh
+++ infrastructure/tactical-rmm/tactical-nginx/init-tactical.sh
@@ -4,11 +4,18 @@ set -e
 # Create required directories if they don't exist
 mkdir -p /var/log/nginx
 mkdir -p /var/run
+mkdir -p /etc/nginx/conf.d
 
 # Set proper permissions
 chmod -R 755 /var/log/nginx
 chown -R nginx:nginx /var/log/nginx
 chown -R nginx:nginx /var/run
+
+# Process main nginx.conf - only replace ${VAR} format
+envsubst '${NGINX_HOST_IP} ${NGINX_HOST_PORT}' < /tmp/nginx.conf > /etc/nginx/nginx.conf
+
+# Process conf.d files - only replace ${VAR} format
+envsubst '${API_HOST} ${APP_HOST} ${NGINX_HOST_IP} ${NGINX_HOST_PORT}' < /tmp/conf.d/default.conf > /etc/nginx/conf.d/default.conf
 
 # Ensure nginx config is valid
 nginx -t
