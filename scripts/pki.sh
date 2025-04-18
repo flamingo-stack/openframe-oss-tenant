5cba147f33f9c741c845383e75f5ca6ba781e123
Vusal Aliyev <vusal@flamingo.cx>
2025-04-18 17:14:07 +0400

    fix curl to work with custom cert

--- scripts/pki.sh
+++ scripts/pki.sh
@@ -6,8 +6,6 @@ if [ -f ${SCRIPT_DIR}/files/ca/ca.key ] && [ -f ${SCRIPT_DIR}/files/ca/ca.crt ];
   if [[ $REPLY =~ ^[Yy]$ ]]; then
     rm -f ${SCRIPT_DIR}/files/ca/ca.key
     rm -f ${SCRIPT_DIR}/files/ca/ca.crt
-  else
-    exit 0
   fi
 fi
 
@@ -23,3 +21,24 @@ openssl req \
   -key ${SCRIPT_DIR}/files/ca/ca.key \
   -out ${SCRIPT_DIR}/files/ca/ca.crt \
   -config ${SCRIPT_DIR}/files/ca/openssl.cnf
+
+# Check if running on Ubuntu and ask to import CA
+# if [ -f /etc/lsb-release ] && grep -q "Ubuntu" /etc/lsb-release; then
+#   read -p "Do you want to import the CA certificate to Ubuntu's trust store? [y/N] " -n 1 -r
+#   echo
+#   if [[ $REPLY =~ ^[Yy]$ ]]; then
+#     # Create a unique name for the cert using its subject hash
+#     CERT_HASH=$(openssl x509 -hash -noout -in ${SCRIPT_DIR}/files/ca/ca.crt)
+
+#     # System-wide CA installation
+#     sudo cp ${SCRIPT_DIR}/files/ca/ca.crt /usr/local/share/ca-certificates/custom_ca.crt
+#     sudo update-ca-certificates
+
+#     # Curl specific CA handling
+#     sudo mkdir -p /etc/ssl/certs
+#     sudo cp ${SCRIPT_DIR}/files/ca/ca.crt /etc/ssl/certs/${CERT_HASH}.0
+#     sudo c_rehash /etc/ssl/certs/
+
+#     echo "CA certificate imported successfully to system and curl trust stores"
+#   fi
+# fi
