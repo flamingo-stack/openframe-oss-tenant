8e310d5e5d14b449c07a300213e107e1909ba025
Vusal Aliyev <vusal@flamingo.cx>
2025-04-21 18:56:56 +0400

    add cacert to curl in register script

--- kind-cluster/apps/openframe-microservices/register/register.sh
+++ kind-cluster/apps/openframe-microservices/register/register.sh
@@ -69,7 +69,7 @@ register_tool() {
     echo "$json_payload" | jq '.'
 
     # Send the request
-    curl -k -X POST "https://openframe-management.192.168.100.100.nip.io/v1/tools/$tool_id" \
+    curl --cacert $SCRIPT_DIR/files/ca/ca.crt -X POST "https://openframe-management.192.168.100.100.nip.io/v1/tools/$tool_id" \
       -H "Content-Type: application/json" \
       -d "$json_payload" \
       --retry 5 \
@@ -382,7 +382,7 @@ register_tool \
     "Integrated Tool" \
     "Integrated Tools" \
     2 \
-    "#455A64",
+    "#455A64" \
     "BEARER_TOKEN"
 
 # Register Authentik with layer info
@@ -480,11 +480,13 @@ echo "Integrated tools initialized successfully!"
 
 # Print Fleet credentials
 echo -e "\nFleet MDM Credentials:"
-echo "Admin User:       admin@openframe.local"
-echo "Admin Password:   openframe123!"
-echo "API User:         api@openframe.local"
-echo "API Password:     openframe123!"
-echo "Fleet API Token:  $FLEET_TOKEN"
+echo "Admin User:           admin@openframe.local"
+echo "Admin Password:       openframe123!"
+echo "API User:             api@openframe.local"
+echo "API Password:         openframe123!"
+echo "Fleet API Token:      $FLEET_TOKEN"
+echo "MeshCentral API Key:  $MESHCENTRAL_API_KEY"
+echo "Authentik API Token:  $AUTHENTIK_API_TOKEN"
 
 echo "OpenFrame is running!"
 echo "Access points:"
