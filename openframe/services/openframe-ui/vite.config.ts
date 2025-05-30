c26633a9cc626bc72114790af39aa9204a770555
Dmytro Koval <dmytro@flamingo.cx>
2025-05-30 19:12:42 +0200

    clean up

--- openframe/services/openframe-ui/vite.config.ts
+++ openframe/services/openframe-ui/vite.config.ts
@@ -16,7 +16,6 @@ export default defineConfig(({ mode }) => {
         protocol: 'wss',
         host: 'openframe-gateway.192.168.100.100.nip.io',
         clientPort: 443,
-        // path: '/ws'
       }
     },
     resolve: {
