40241f97b07498ee12877a89c456f6b8233f6171
Arsenii-Malov <arsenij@flamingo.cx>
2025-06-16 15:47:34 +0000

    MeshCentral. Kafka-to-Cassandra Message Processing Service (#296)

---
+++ openframe/libs/openframe-data/src/main/java/com/openframe/data/model/cassandra/CassandraITEventEntity.java
@@ -0,0 +1,38 @@
+package com.openframe.data.model.cassandra;
+
+import com.openframe.data.model.DownstreamEntity;
+import lombok.Data;
+import org.springframework.data.cassandra.core.cql.Ordering;
+import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
+import org.springframework.data.cassandra.core.mapping.*;
+
+import java.time.Instant;
+import java.util.Map;
+
+@Table("integrated_tool_event")
+@Data
+public class CassandraITEventEntity implements DownstreamEntity {
+
+    @PrimaryKey
+    private CassandraITEventEntity.CassandraITEventKey key;
+
+    @Column("payload")
+    private Map<String, String> payload;
+
+    @Column("event_type")  // Fixed column name to match database
+    private String eventType;
+
+    @PrimaryKeyClass
+    @Data
+    public static class CassandraITEventKey {
+        @PrimaryKeyColumn(name = "id", type = PrimaryKeyType.PARTITIONED)
+        private String id;
+
+        @PrimaryKeyColumn(name = "timestamp", ordinal = 0, ordering = Ordering.DESCENDING)
+        private Instant timestamp;
+
+        @PrimaryKeyColumn(name = "tool_name", ordinal = 1, ordering = Ordering.ASCENDING)
+        private String toolName;
+    }
+
+}
