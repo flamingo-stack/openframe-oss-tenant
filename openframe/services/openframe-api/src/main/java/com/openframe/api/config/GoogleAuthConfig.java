045e9e3c8da3837d9d02dcc47f6850d42cd6180b
aliaska-varieva <aliaska@flamingo.cx>
2025-06-18 00:12:34 +0200

    Delete GoogleAuthConfig

--- openframe/services/openframe-api/src/main/java/com/openframe/api/config/GoogleAuthConfig.java
+++
@@ -1,9 +0,0 @@
-package com.openframe.api.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class GoogleAuthConfig {
-    // GoogleIdTokenVerifier тепер створюється динамічно в GoogleAuthStrategy
-    // на основі конфігурації з бази даних
-} 
