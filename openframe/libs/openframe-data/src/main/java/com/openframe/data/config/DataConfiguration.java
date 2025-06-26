ef00254defa423a2cee67eefad90470f8190b215
Arsenij Malov <arsenij@flamingo.cx>
2025-06-26 20:56:08 +0300

    small fixes

--- openframe/libs/openframe-data/src/main/java/com/openframe/data/config/DataConfiguration.java
+++ openframe/libs/openframe-data/src/main/java/com/openframe/data/config/DataConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @Configuration
@@ -13,7 +12,6 @@ public class DataConfiguration {
     @Configuration
     @ConditionalOnProperty(name = "spring.data.mongodb.enabled", havingValue = "true", matchIfMissing = false)
     @EnableMongoRepositories(basePackages = "com.openframe.data.repository.mongo")
-    @EnableReactiveMongoRepositories(basePackages = "com.openframe.data.repository.mongo")
     public static class MongoConfiguration {}
 
     @Configuration
