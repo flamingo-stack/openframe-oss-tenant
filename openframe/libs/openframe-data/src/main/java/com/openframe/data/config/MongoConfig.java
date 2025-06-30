334e220c06c4520059f231a7e277dc7dce1588d8
Arsenii-Malov <arsenij@flamingo.cx>
2025-06-30 16:02:16 +0300

    Returned back mongo config (#317)

---
+++ openframe/libs/openframe-data/src/main/java/com/openframe/data/config/MongoConfig.java
@@ -0,0 +1,13 @@
+package com.openframe.data.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@ConditionalOnProperty(name = "spring.data.mongodb.enabled", havingValue = "true", matchIfMissing = false)
+@EnableMongoRepositories(basePackages = "com.openframe.data.repository.mongo")
+@EnableReactiveMongoRepositories(basePackages = "com.openframe.data.repository.mongo")
+public class MongoConfig {
+}
