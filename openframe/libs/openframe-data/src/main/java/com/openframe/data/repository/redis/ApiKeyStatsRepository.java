c2f1935e9eaefef3206cef5b2632a964206de554
aliaska-varieva <aliaska@flamingo.cx>
2025-07-04 14:11:01 +0200

    Provide code refactoring

--- openframe/libs/openframe-data/src/main/java/com/openframe/data/repository/redis/ApiKeyStatsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.openframe.data.repository.redis;
-
-import com.openframe.data.model.redis.ApiKeyStats;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ApiKeyStatsRepository extends CrudRepository<ApiKeyStats, String> {
-} 
--- openframe/libs/openframe-data/src/main/java/com/openframe/data/repository/redis/RateLimitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.openframe.data.repository.redis;
-
-import com.openframe.data.model.redis.RateLimit;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RateLimitRepository extends CrudRepository<RateLimit, String> {
-} 
