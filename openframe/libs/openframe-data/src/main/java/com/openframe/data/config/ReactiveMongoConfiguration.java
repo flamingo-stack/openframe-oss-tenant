1e2354604cd3a4ff0254e5575435be23faa48814
Arsenij Malov <arsenij@flamingo.cx>
2025-07-21 14:52:07 +0300

    setMapKeyDotReplacement to reactive mongo config

--- openframe/libs/openframe-data/src/main/java/com/openframe/data/config/ReactiveMongoConfiguration.java
+++ openframe/libs/openframe-data/src/main/java/com/openframe/data/config/ReactiveMongoConfiguration.java
@@ -4,7 +4,13 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.convert.DbRefResolver;
+import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
@@ -15,5 +21,16 @@ public class ReactiveMongoConfiguration {
     @Bean
     public ReactiveMongoTemplate reactiveMongoTemplate(MongoClient mongoClient) {
         return new ReactiveMongoTemplate(mongoClient, "openframe");
+    }
+
+    @Bean
+    public MappingMongoConverter reactiveMappingMongoConverter(MongoDatabaseFactory factory,
+                                                               MongoMappingContext context,
+                                                               MongoCustomConversions conversions) {
+        DbRefResolver dbRefResolver = new DefaultDbRefResolver(factory);
+        MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, context);
+        converter.setCustomConversions(conversions);
+        converter.setMapKeyDotReplacement("__dot__");
+        return converter;
     }
 } 
