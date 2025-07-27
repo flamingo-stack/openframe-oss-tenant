e5d3a64ca9f55af46c3389ca716969799b9efbeb
Arsenij Malov <arsenij@flamingo.cx>
2025-07-27 22:45:28 +0300

    update debezium

--- openframe/services/openframe-stream/src/main/java/com/openframe/stream/service/HostAgentCacheService.java
+++ openframe/services/openframe-stream/src/main/java/com/openframe/stream/service/HostAgentCacheService.java
@@ -1,13 +1,18 @@
 package com.openframe.stream.service;
 
+import com.openframe.core.model.IntegratedTool;
+import com.openframe.core.model.IntegratedToolId;
+import com.openframe.data.service.IntegratedToolService;
 import com.openframe.sdk.fleetmdm.FleetMdmClient;
 import com.openframe.sdk.fleetmdm.model.Host;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Service for host-agent cache operations using Spring Cache abstraction
@@ -19,7 +24,12 @@ import java.io.IOException;
 @Slf4j
 public class HostAgentCacheService {
 
-    private final FleetMdmClient fleetMdmClient;
+    @Value("${fleet.mdm.base-url:http://fleetmdm-server.integrated-tools.svc.cluster.local:8070}")
+    private String baseUrl;
+
+    private FleetMdmClient fleetMdmClient;
+
+    private final IntegratedToolService integratedToolService;
 
     /**
      * Get agent ID from cache or Fleet MDM API
@@ -31,11 +41,21 @@ public class HostAgentCacheService {
     public String getAgentId(Integer hostId) {
         log.debug("Fetching agent ID for host: {}", hostId);
         try {
-            Host host = fleetMdmClient.getHostById(hostId.longValue());
+            Host host = getFleetMdmClient() != null ? this.fleetMdmClient.getHostById(hostId.longValue()) : null;
             return host != null ? host.getUuid() : null;
         } catch (IOException | InterruptedException e) {
             log.error("Error fetching agent ID for host: {}", hostId, e);
             return null;
         }
+    }
+
+    private FleetMdmClient getFleetMdmClient() {
+        if (fleetMdmClient == null) {
+            Optional<IntegratedTool> optionalFleetInfo = integratedToolService.getToolById(IntegratedToolId.FLEET_SERVER_ID.getValue());
+            optionalFleetInfo.ifPresent(integratedTool -> {
+                this.fleetMdmClient = new FleetMdmClient(baseUrl, integratedTool.getCredentials().getApiKey().getKey());
+            });
+        }
+        return fleetMdmClient;
     }
 } 
