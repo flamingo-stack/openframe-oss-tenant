f3a428460bc648742cf8576f06e9dea9c0c0c3e5
aliaska-varieva <aliaska@flamingo.cx>
2025-08-14 00:17:50 +0200

    Fix dashboard (#389)

--- openframe/services/openframe-ui/src/graphql/queries.ts
+++ openframe/services/openframe-ui/src/graphql/queries.ts
@@ -1,40 +1,42 @@
 import { gql } from '@apollo/client/core';
 
 export const GET_INTEGRATED_TOOLS = gql`
-  query GetIntegratedTools($filter: ToolFilter) {
-    integratedTools(filter: $filter) {
-      id
-      name
-      description
-      icon
-      toolUrls {
-        url
-        port
-        type
-      }
-      type
-      toolType
-      category
-      platformCategory
-      enabled
-      credentials {
-        username
-        password
-        apiKey {
-          key
+  query GetIntegratedTools($filter: ToolFilterInput, $search: String) {
+    integratedTools(filter: $filter, search: $search) {
+      tools {
+        id
+        name
+        description
+        icon
+        toolUrls {
+          url
+          port
           type
-          keyName
         }
+        type
+        toolType
+        category
+        platformCategory
+        enabled
+        credentials {
+          username
+          password
+          apiKey {
+            key
+            type
+            keyName
+          }
+        }
+        layer
+        layerOrder
+        layerColor
+        metricsPath
+        healthCheckEndpoint
+        healthCheckInterval
+        connectionTimeout
+        readTimeout
+        allowedEndpoints
       }
-      layer
-      layerOrder
-      layerColor
-      metricsPath
-      healthCheckEndpoint
-      healthCheckInterval
-      connectionTimeout
-      readTimeout
-      allowedEndpoints
     }
   }
 `;
