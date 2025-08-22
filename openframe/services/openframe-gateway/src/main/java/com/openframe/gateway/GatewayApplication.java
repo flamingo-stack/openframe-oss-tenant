cf7445efbc3e5422cfe7b1aa767a388cb6f881ca
Kirill Gontar <Kirill Gontar>
2025-08-23 01:29:04 +0200

    TODO

--- openframe/services/openframe-gateway/src/main/java/com/openframe/gateway/GatewayApplication.java
+++ openframe/services/openframe-gateway/src/main/java/com/openframe/gateway/GatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import lombok.RequiredArgsConstructor;
 
 @SpringBootApplication
+// TODO: remove scheduling ?
 @EnableScheduling
 @ComponentScan(basePackages = {"com.openframe.gateway", "com.openframe.core", "com.openframe.data", "com.openframe.security"})
 @RequiredArgsConstructor
