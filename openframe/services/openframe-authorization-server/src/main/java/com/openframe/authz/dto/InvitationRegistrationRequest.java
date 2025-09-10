6ffdd7f45d5b6134b18000b1566866bcd5d5010f
aliaska-varieva <aliaska@flamingo.cx>
2025-09-11 01:40:29 +0200

    Add registration and invitation functionality, get users, get invitation endpoints

---
+++ openframe/services/openframe-authorization-server/src/main/java/com/openframe/authz/dto/InvitationRegistrationRequest.java
@@ -0,0 +1,18 @@
+package com.openframe.authz.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class InvitationRegistrationRequest {
+    @NotBlank
+    private String invitationId;
+
+    @NotBlank
+    private String password;
+
+    private String firstName;
+    private String lastName;
+}
+
+
