caf86e6e34a43c7036a57c6867548a744c473533
aliaska-varieva <aliaska@flamingo.cx>
2025-09-17 00:32:12 +0200

    Add annotation based tenant domain validation

--- openframe/services/openframe-authorization-server/src/main/java/com/openframe/authz/dto/TenantRegistrationRequest.java
+++ openframe/services/openframe-authorization-server/src/main/java/com/openframe/authz/dto/TenantRegistrationRequest.java
@@ -1,14 +1,13 @@
 package com.openframe.authz.dto;
 
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
+import com.openframe.authz.validation.TenantDomain;
+import com.openframe.core.validation.ValidEmail;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-import com.openframe.authz.validation.TenantDomain;
 
 /**
  * User registration request DTO for multi-tenant registration
@@ -19,19 +18,14 @@ import com.openframe.authz.validation.TenantDomain;
 @NoArgsConstructor
 @AllArgsConstructor
 public class TenantRegistrationRequest extends CoreUserRequest {
-    
-    @NotBlank(message = "Email is required")
-    @Email(
-            regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*\\.[A-Za-z]{2,}$",
-            message = "Invalid email format"
-    )
+
+    @ValidEmail
     private String email;
     
     /**
      * Organization/tenant name for registration
      * This will be used to create a new tenant if it doesn't exist
      */
-    @NotBlank(message = "Organization name is required")
     @Pattern(
             regexp = "^[\\p{L}\\p{M}0-9&.,'’\"()\\- ]{2,100}$",
             message = "Invalid organization name"
@@ -41,7 +35,6 @@ public class TenantRegistrationRequest extends CoreUserRequest {
     /**
      * Tenant domain
      */
-    @NotBlank(message = "Tenant domain is required")
     @TenantDomain
     private String tenantDomain;
 }
