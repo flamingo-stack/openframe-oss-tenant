5ff650f4a7eaa8f0833fc1df7ec63a3e8411edec
Arsenij Malov <arsenij@flamingo.cx>
2025-11-06 11:01:56 +0200

    openframe-client moved to oss-lib

--- openframe/services/openframe-client/src/main/java/com/openframe/client/dto/client/CreateClientRequest.java
+++
@@ -1,9 +0,0 @@
-package com.openframe.client.dto.client;
-
-import lombok.Data;
-
-@Data
-public class CreateClientRequest {
-    private String[] grantTypes;
-    private String[] scopes;
-} 
--- openframe/services/openframe-client/src/main/java/com/openframe/client/exception/InvalidToolTypeException.java
+++
@@ -1,7 +0,0 @@
-package com.openframe.client.exception;
-
-public class InvalidToolTypeException extends RuntimeException {
-    public InvalidToolTypeException(String message) {
-        super(message);
-    }
-}
