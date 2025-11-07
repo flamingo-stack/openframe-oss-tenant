6eda29206d7719bf22ad2909e6a55782e9f50d21
aliaska-varieva <aliaska@flamingo.cx>
2025-11-07 11:54:45 +0100

    Publish to machine.*.tool-update specific agentId instead of machine.all.tool-update

--- openframe/services/openframe-management/src/main/java/com/openframe/management/service/ToolAgentUpdatePublisher.java
+++ openframe/services/openframe-management/src/main/java/com/openframe/management/service/ToolAgentUpdatePublisher.java
@@ -6,6 +6,7 @@ import com.openframe.data.document.toolagent.IntegratedToolAgent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 import static java.lang.String.format;
 
@@ -14,13 +15,16 @@ import static java.lang.String.format;
 @Slf4j
 public class ToolAgentUpdatePublisher {
 
-    private final static String TOPIC_NAME_TEMPLATE = "machine.all.tool-update";
+    private final static String TOPIC_NAME_TEMPLATE = "machine.%s.tool-update";
 
     private final NatsMessagePublisher natsMessagePublisher;
 
     public void publish(IntegratedToolAgent toolAgent) {
         ToolAgentUpdateMessage message = buildMessage(toolAgent);
-        natsMessagePublisher.publish(TOPIC_NAME_TEMPLATE, message);
+
+        String topicName = format(TOPIC_NAME_TEMPLATE, toolAgent.getId());
+        natsMessagePublisher.publish(topicName, message);
+
         log.info("Published tool update message for tool: {} version: {}", toolAgent.getId(), toolAgent.getVersion());
     }
 
@@ -28,6 +32,12 @@ public class ToolAgentUpdatePublisher {
         ToolAgentUpdateMessage message = new ToolAgentUpdateMessage();
         message.setToolAgentId(toolAgent.getId());
         message.setVersion(toolAgent.getVersion());
+
+        List<String> formattedLinks =  toolAgent.getDownloadLinks().stream()
+                    .map(link -> format(link, toolAgent.getVersion()))
+                    .toList();
+        message.setLinks(formattedLinks);
+
         return message;
     }
 }
