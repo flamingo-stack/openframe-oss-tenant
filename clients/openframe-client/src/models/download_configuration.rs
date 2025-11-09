b18fb95cd62c8726ee102ce00814db47a2af3e29
Kirill Gontar <Kirill Gontar>
2025-11-09 01:27:10 +0100

    Fixes

--- clients/openframe-client/src/models/download_configuration.rs
+++ clients/openframe-client/src/models/download_configuration.rs
@@ -1,6 +1,7 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct DownloadConfiguration {
     pub os: String,
     pub file_name: String,
