f7f4b2b4717abee24ea966e70b443f935bd463ec
giokur <george.kurtanidze+1@gmail.com>
2026-01-29 14:55:00 +0100

    add Devices tests

--- openframe-e2e-tests/src/test/java/com/openframe/config/ApiConfig.java
+++ openframe-e2e-tests/src/test/java/com/openframe/config/ApiConfig.java
@@ -6,8 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ApiConfig {
 
-    //    public static final String DEFAULT_BASE_URL = "https://localhost/";
-    public static final String DEFAULT_BASE_URL = "https://dmytro.openframe.build/";
+    public static final String DEFAULT_BASE_URL = "https://localhost/";
 
     private static String baseUrl;
 
