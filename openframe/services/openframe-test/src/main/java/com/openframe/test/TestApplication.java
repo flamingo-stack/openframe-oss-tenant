c72c90dcaff9067fe164d138e3734bc539a85973
giokur <george.kurtanidze+1@gmail.com>
2026-03-03 21:13:45 +0100

    add user tests

--- openframe/services/openframe-test/src/main/java/com/openframe/test/TestApplication.java
+++ openframe/services/openframe-test/src/main/java/com/openframe/test/TestApplication.java
@@ -5,11 +5,17 @@ import com.openframe.test.runner.TestRunner;
 import com.openframe.test.runner.TestRunnerConfig;
 import io.qameta.allure.junitplatform.AllureJunitPlatform;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 
 import static com.openframe.test.SummaryLogger.logSummary;
 import static com.openframe.test.SummaryLogger.logTestList;
+import static org.junit.platform.engine.discovery.ClassNameFilter.excludeClassNamePatterns;
+import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+import static org.junit.platform.launcher.TagFilter.includeTags;
 
 @Slf4j
 public class TestApplication {
@@ -38,11 +44,45 @@ public class TestApplication {
         }
         if (testsPassed) {
             log.info("Run OSS tests");
-            TestPlan testPlan = testRunner.discover("oss");
+            LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(selectPackage(config.getTestPackage()))
+                    .filters(includeTags("oss"), excludeClassNamePatterns("UserInvitationsTest"))
+                    .build();
+            TestPlan testPlan = testRunner.discover(request);
             logTestList(testRunner.list(testPlan));
             testRunner.run(testPlan);
             logSummary(summaryListener.getSummary());
             testsPassed = summaryListener.getSummary().getTestsFailedCount() == 0;
+
+            request = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(selectPackage(config.getTestPackage()))
+                    .filters(includeClassNamePatterns("UserTest"))
+                    .build();
+            testPlan = testRunner.discover(request);
+            logTestList(testRunner.list(testPlan));
+            testRunner.run(testPlan);
+            logSummary(summaryListener.getSummary());
+            testsPassed = testsPassed && summaryListener.getSummary().getTestsFailedCount() == 0;
+
+            request = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(selectPackage(config.getTestPackage()))
+                    .filters(includeClassNamePatterns("UserInvitationsTest"))
+                    .build();
+            testPlan = testRunner.discover(request);
+            logTestList(testRunner.list(testPlan));
+            testRunner.run(testPlan);
+            logSummary(summaryListener.getSummary());
+            testsPassed = testsPassed && summaryListener.getSummary().getTestsFailedCount() == 0;
+
+            request = LauncherDiscoveryRequestBuilder.request()
+                    .selectors(selectPackage(config.getTestPackage()))
+                    .filters(includeClassNamePatterns("ResetPasswordTest"))
+                    .build();
+            testPlan = testRunner.discover(request);
+            logTestList(testRunner.list(testPlan));
+            testRunner.run(testPlan);
+            logSummary(summaryListener.getSummary());
+            testsPassed = testsPassed && summaryListener.getSummary().getTestsFailedCount() == 0;
         } else {
             log.error("Registration tests failed - interrupting execution");
         }
