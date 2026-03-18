0238d949e5e8041db757cf3bebb54cbb22d44efa
yevhen-maker <yevhen@flamingo.cx>
2026-03-18 18:33:31 +0300

    fix tags graphql fields for device filters

--- openframe/services/openframe-frontend/src/app/devices/types/device.types.ts
+++ openframe/services/openframe-frontend/src/app/devices/types/device.types.ts
@@ -278,8 +278,8 @@ export interface DeviceFilters {
   statuses: DeviceFilterValue[];
   deviceTypes: DeviceFilterValue[];
   osTypes: DeviceFilterValue[];
-  organizationIds: DeviceFilterTag[];
-  tags: DeviceFilterTag[];
+  organizationIds: DeviceFilterValue[];
+  tagKeys: DeviceFilterTag[];
   filteredCount: number;
 }
 
@@ -288,7 +288,8 @@ export interface DeviceFilterInput {
   deviceTypes?: string[];
   osTypes?: string[];
   organizationIds?: string[];
-  tags?: string[];
+  tagKeys?: string[];
+  tagValues?: string[];
 }
 
 export interface GraphQlResponse<T> {
