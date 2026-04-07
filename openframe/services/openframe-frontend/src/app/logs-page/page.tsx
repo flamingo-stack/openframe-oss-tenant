0c1de0a81435b68440140337804f14acf415cb90
Pavlo Shylo <pavlo@flamingo.cx>
2026-04-07 22:01:45 +0100

    fix logs page

--- openframe/services/openframe-frontend/src/app/logs-page/page.tsx
+++ openframe/services/openframe-frontend/src/app/logs-page/page.tsx
@@ -3,13 +3,13 @@
 export const dynamic = 'force-dynamic';
 
 import { AppLayout } from '../components/app-layout';
-import { LogsTableRelay } from './components/logs-table-relay';
+import { LogsTable } from './components/logs-table';
 
 export default function Logs() {
   return (
     <AppLayout>
       <div className="space-y-6">
-        <LogsTableRelay />
+        <LogsTable />
       </div>
     </AppLayout>
   );
